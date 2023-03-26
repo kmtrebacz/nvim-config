2ab6c355b586fdc95372071fbd54d35b96ca558b
Kacper <kacperkapitrebacz@gmail.com>
2023-03-26 20:36:47 +0200

    Add mason and nvim-cmp

---
+++ lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,18 @@
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+local on_attach = function(_, _)
+     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- After setting up mason-lspconfig you may set up servers via lspconfig
+require("lspconfig").lua_ls.setup {
+     on_attach =  on_attach,
+     capabilities = capabilities,
+}
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -17,7 +17,16 @@ local ensure_packer = function()
     use 'nvim-tree/nvim-tree.lua'
     use 'nvim-lualine/lualine.nvim'
     use 'nvim-tree/nvim-web-devicons'
-    
+    use {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    }
+    -- completion
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'L3MON4D3/LuaSnip'
+
      -- Automatically set up your configuration after cloning packer.nvim
      -- Put this at the end after all plugins
      if packer_bootstrap then
