f24d61dfa07c215500b735b5dd9814e88ede70ad
Kacper <kacperkapitrebacz@gmail.com>
2023-08-20 23:47:20 +0200

    changes in lualine

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -2,14 +2,14 @@ require('lualine').setup {
      options = {
           icons_enabled = true,
           theme = "sonokai",
-          component_separators = { left = '', right = ''},
-    	  section_separators = { left = '', right = ''},
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
      },
      sections = {
           lualine_a = {'mode'},
           lualine_b = {'branch', 'diff', 'diagnostics'},
           lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_x = {'fileformat', 'filetype'},
           lualine_y = {'progress'},
           lualine_z = {'location'}
         },
