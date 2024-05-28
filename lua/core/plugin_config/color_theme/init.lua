852208919c3bf111a58af446da09a5bed0a356ce
kmtrebacz <119664516+kmtrebacz@users.noreply.github.com>
2024-05-28 14:52:52 +0200

    mmm... another font change

--- lua/core/plugin_config/color_theme/init.lua
+++ lua/core/plugin_config/color_theme/init.lua
@@ -1,2 +1,13 @@
+require("gruvbox").setup({
+  bold = false,
+  italic = {
+    strings = false,
+    emphasis = false,
+    comments = false,
+    operators = false,
+    folds = false,
+  },
+})
+
 vim.opt.termguicolors = true
-vim.cmd.colorscheme 'doom-one'
+vim.cmd.colorscheme 'gruvbox'
